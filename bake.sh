85159c5732fb7ca03b487521494db93ac60a5c7b
saintabomination <saint.abomination@protonmail.com>
2022-05-29 15:12:31 +0200

    Adding SFML to the project

--- bake.sh
+++ bake.sh
@@ -2,10 +2,11 @@
 
 PROJECT_NAME="sfml-craft"
 COMPILER="g++"
+LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system"
 SRC_DIR="src"
 BUILD_DIR="build"
 
 $COMPILER -c $SRC_DIR/Main.cpp
 mkdir -p $BUILD_DIR
-$COMPILER *.o -o $BUILD_DIR/$PROJECT_NAME
+$COMPILER *.o -o $BUILD_DIR/$PROJECT_NAME $LIBRARIES
 rm -rf *.o
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include "Headers.h"
 
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
+  sf::CircleShape shape(128.f);
+  shape.setFillColor(sf::Color::Red);
+
+  while (window.isOpen())
+  {
+    sf::Event sfEvent;
+    while (window.pollEvent(sfEvent))
+    {
+      switch (sfEvent.type)
+      {
+        case sf::Event::Closed:
+          window.close();
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
   return 0;
 }
